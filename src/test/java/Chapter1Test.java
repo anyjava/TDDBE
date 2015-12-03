85da2f5dbd62a427c88ff58187b3e492c3dc39bd
손현태 <htson@woowahan.com>
2015-12-03 13:06:17 +0900

    3장 프라이버시 실습

--- src/test/java/Chapter1Test.java
+++ src/test/java/Chapter1Test.java
@@ -4,17 +4,24 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+// TODO: 5$ + 10CHF = $10
+// DONE: $5 * 2
+// DONE: amount 를 private 으로 만들기
+// DONE: Dollar 부작용
+// TODO: Money 반올림
+// DONE: equals()
+// TODO: hashCode()
+// TODO: Equal null
+// TODO: Equal Object
 public class Chapter1Test {
 
     @Test
     public void testMultiplication() {
         Dollar five    = new Dollar(5);
-        Dollar product = five.times(2);
 
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
 
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
